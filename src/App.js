f831409d0694da0c747b5a4750a89e8664d7a214
lintopher0315 <lin978@purdue.edu>
2020-07-20 21:00:02 -0400

    added layout for header, home, footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
+import './components/styles.css';
 
 import Home from './components/Home';
+import Header from './components/Header';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <Route path="/" component={Home} />
-      </div>
-    </BrowserRouter>
+    <div id="app">
+      <Header />
+      <BrowserRouter>
+        <div>
+          <Route path="/" component={Home} />
+        </div>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function Home() {
     return (
-        "Hello"
+        <div>
+
+        </div>
     )
 }
 
