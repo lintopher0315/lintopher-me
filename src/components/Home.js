9494a67895e9625eac443b6ce205a73d6daee1ec
lintopher0315 <lin978@purdue.edu>
2020-07-20 21:44:51 -0400

    added grid system for images and info

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
+const IMGS = [
+    ["https://picsum.photos/300/200", "https://picsum.photos/300/200", "https://picsum.photos/300/200"],
+    ["https://picsum.photos/300/200", "https://picsum.photos/300/200", "https://picsum.photos/300/200"],
+    ["https://picsum.photos/300/200", null, null],
+]
+
 function Home() {
+
+    let grid = IMGS.map((img, i) => {
+        return (
+            <Row id="row">
+                <Col>
+                    <img src={img[0]} />
+                </Col>
+                <Col>
+                    <img src={img[1]} />
+                </Col>
+                <Col>
+                    <img src={img[2]} />
+                </Col>
+            </Row>
+        )
+    })
+
     return (
         <div>
-
+            <Container fluid>
+                {grid}
+            </Container>
         </div>
     )
 }
