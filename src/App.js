121426c6451fac08ba99a9ec73967d5d38b253d5
lintopher0315 <lin978@purdue.edu>
2020-07-22 20:49:41 -0400

    added footer icons and text

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './components/styles.css';
 
 import Home from './components/Home';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" component={Home} />
         </div>
       </BrowserRouter>
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,24 @@ import React from 'react';
 
 function Footer() {
     return (
-        <div>
-            hello
+        <div id="footer">
+            <div id="icon-container">
+                <div id="footer-div">
+                    <img src={require("./../res/github.svg")} alt="1" id="footer-icon" />
+                </div>
+                <div id="footer-div">
+                    <img src={require("./../res/mail.svg")} alt="1" id="footer-icon" />
+                </div>
+                <div id="footer-div">
+                    <img src={require("./../res/linkedin.svg")} alt="1" id="footer-icon" />
+                </div>
+                <div id="footer-div">
+                    <img src={require("./../res/leetcode.svg")} alt="1" id="footer-icon" />
+                </div>
+            </div>
+            <div id="footer-text-container">
+                <p id="copyright-text">&copy; Christopher Lin 2020</p>
+            </div>
         </div>
     )
 }
